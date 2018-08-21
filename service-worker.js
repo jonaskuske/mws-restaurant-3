6c91881e25ea153e0ced8c462237351f07a09bd4
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-08-21 22:53:57 +0200

    feat: sw - dont cache backend requests service worker skips caching for requests to the backend, as the DBHelper will get its own caching mechanism also skip caching of opaque responses as that leads to quota issues

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const cachename = 'restaurant-reviews-v1';
+const BACKEND_HOST = 'localhost:1337';
 
 const assets = [
     '.',
@@ -10,7 +11,6 @@ const assets = [
     './js/main.js',
     './js/dbhelper.js',
     './js/restaurant_info.js',
-    './data/restaurants.json',
     './img/1.jpg',
     './img/2.jpg',
     './img/3.jpg',
@@ -24,6 +24,22 @@ const assets = [
     './img/bg.jpg',
 ];
 
+/* Helper functions to dtermine whether requests/responses should be cached */
+const isRequestCacheable = request => {
+    const url = new URL(request.url);
+    // don't cache responses from backend as it has its own caching using idb
+    if (url.host === BACKEND_HOST) return false;
+
+    return true;
+}
+const isResponseCacheable = response => {
+    // don't cache opaque response to prevent exceeding cache size quota
+    // see https://cloudfour.com/thinks/when-7-kb-equals-7-mb/
+    if (response.status === 0 || response.type === 'opaque') return false;
+
+    return true
+}
+
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(cachename).then((cache) => {
@@ -33,8 +49,14 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
+    // if request should not be cached: respond with fetch and return
+    if (!isRequestCacheable(e.request)) {
+        e.respondWith(fetch(e.request));
+        return;
+    }
 
-    // neccessary for sw to handle requests with query strings ( /restaurant.html?id=1 )
+    // neccessary for sw to handle requests with query strings
+    // like /restaurant.html?id=1
     const request = e.request.url.includes('/restaurant.html') ?
         new Request('/restaurant.html') :
         e.request;
@@ -46,8 +68,14 @@ self.addEventListener('fetch', (e) => {
                 // if online: fetch ressource and update cache asynchronously
                 if (navigator.onLine) {
                     fetch(request).then((netresponse) => {
-                        caches.open(cachename).then((cache) => cache.put(request, netresponse))
-                    }).catch((e) => { /* catch DevTools related only-if-cached error */ });
+                        if (isResponseCacheable(netresponse)) {
+                            caches
+                                .open(cachename)
+                                .then((cache) => cache.put(request, netresponse))
+                        }
+                    }).catch((e) => {
+                        /* catch DevTools related only-if-cached error */
+                    });
                 }
 
                 // immediately return cached response: offline-first 🤘
@@ -58,7 +86,12 @@ self.addEventListener('fetch', (e) => {
                 // fetch previously uncached ressource, cache cloned response, return real response
                 return fetch(request).then((response) => {
                     const cacheResponse = response.clone();
-                    caches.open(cachename).then((cache) => cache.put(request, cacheResponse));
+
+                    if (isResponseCacheable(cacheResponse)) {
+                        caches
+                            .open(cachename)
+                            .then((cache) => cache.put(request, cacheResponse));
+                    }
 
                     return response;
                 });
